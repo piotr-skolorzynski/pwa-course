a1254f27368b80dc1fc86ffd97598ea6e0283497
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-12-25 18:08:01 +0100

    modify SW in sw.js file add listeners for install, activate and fetch events

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,2 +1,19 @@
+//lifecycle events
+self.addEventListener('install', (event) => {
+    console.log('[Service Worker]: Installing Service Worker ..', event);
+});
+self.addEventListener('activate', (event) => {
+    console.log('[Service Worker]: Activating Service Worker ...', event);
+
+    return self.clients.claim();
+});
+
+//non-lifecycle events
+self.addEventListener('fetch', (event) => {
+    console.log('[Service Worker] Fetching something ...', event);
+    // event.respondWith(null); //zwróci null więc strona padnie ;)
+    event.respondWith(fetch(event.request)); //zwróci niezmienione dane
+});
+
 
 
