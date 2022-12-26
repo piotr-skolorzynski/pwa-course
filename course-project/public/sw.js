a353368e8c63ca634e8b0ea7a783895cce6a35f1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-12-26 14:21:56 +0100

    modify sw.js add dynamic versioning and cleaning cache

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,7 +1,11 @@
+let CACHE_STATIC_NAME = 'static-v4';
+let CACHE_DYNAMIC_NAME = 'dynamic-v2'
+
 //lifecycle events
 self.addEventListener('install', (event) => {
     console.log('[Service Worker]: Installing Service Worker ..', event);
-    event.waitUntil(caches.open('static').then((cache) => {
+
+    event.waitUntil(caches.open(CACHE_STATIC_NAME).then((cache) => {
         console.log('[Service Worker]: Precaching app shell!', cache);
         cache.addAll([
             '/',
@@ -21,20 +25,36 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     console.log('[Service Worker]: Activating Service Worker ...', event);
+    //wykonanie czyszczenia caches
+    //caches.keys() - zwraca wszystkie klucze cache jaki istnieją
+    event.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                console.log('[Service Worker]: Removing old cache!', key);
+                return caches.delete(key); //usunięcie starego cache'a
+            }
+        }))
+    }));
 
     return self.clients.claim();
 });
 
 //non-lifecycle events
 self.addEventListener('fetch', (event) => {
-    //event.request - nazwa klucza dla cache, dla fetch jest to adres
-    //sprawdzamy czy mamy w cache'u pliki, jeśli tak to je zasysamy jak nie to wykonujemy fetch
-    //później będzie inny przykład, jeśli nie ma połączenia i nie jesteśmy w stanie pobrać danych to skorzystaj z tych, które są w cache'u.
+    //zapisanie do cache'a dynamicznie danych które zostały pobrane z sieci
     event.respondWith(caches.match(event.request).then((respone) => {
         if (respone) {
             return respone;
         } else {
-            return fetch(event.request);
+            return fetch(event.request)
+                .then((respone) => {
+                    return caches.open(CACHE_DYNAMIC_NAME)
+                        .then((cache) => {
+                            cache.put(event.request.url, respone.clone()); //put nie w przeciwnieństwie do add nie wysyła requesta i automatycznie zachowuje parę klucz-wartość 
+                            //resposne jest konsumowana jednorazowo i dalej jest pust więc dlatego musi być użyta metoda .clone() żeby zapisać do cache i jeszce móć zwrócić response.
+                            return respone;
+                        });
+                });
         }
     }));
 });
